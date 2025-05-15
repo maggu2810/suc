b7d3a551dfb514f8e9bc4b855a2cae5354086049
Markus Rathgeb <maggu2810@mailbox.org>
2025-05-15 21:23:09 +0200

    update copyright
    Signed-off-by: Markus Rathgeb <maggu2810@mailbox.org>

--- cmn/src/cleanup.cxx
+++ cmn/src/cleanup.cxx
@@ -1,4 +1,4 @@
-// Copyright [2024] [maggu2810]
+// Copyright [2024-2025] [maggu2810]
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
