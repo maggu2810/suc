08845a01c4cf0f614d64e0d6f99f55e9100bfd53
Markus Rathgeb <maggu2810@mailbox.org>
2026-01-31 11:57:37 +0100

    epl: signal: add signal handling support
    Signed-off-by: Markus Rathgeb <maggu2810@mailbox.org>

---
+++ epl/include/suc/epl/common/Signal.hxx
@@ -0,0 +1,57 @@
+// Copyright [2026] [maggu2810]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+// Created by maggu2810 on 1/30/26.
+//
+#ifndef SUC_EPL_SIGNAL_HXX
+#define SUC_EPL_SIGNAL_HXX
+
+#include "suc/epl/base/Fd.hxx"
+#include <functional>
+#include <sys/signalfd.h>
+
+// #error "Never include <bits/signum-generic.h> directly; use <signal.h> instead."
+// signals: <bits/signum-generic.h>
+// signals: <bits/signum-arch.h>
+
+namespace suc::epl {
+    class Signal {
+    private:
+        [[nodiscard]] static sigset_t createSigSet(std::initializer_list<int> signals);
+
+        static void blockSignals(const sigset_t& sigset);
+
+        explicit Signal(const sigset_t& sigset, EventQueue& eventQueue = EventQueue::coreInstance());
+
+        explicit Signal(const sigset_t& sigset, bool block = true, EventQueue& eventQueue = EventQueue::coreInstance())
+            : Signal(sigset, eventQueue) {
+            if (block) {
+                blockSignals(sigset);
+            }
+        }
+
+    public:
+        explicit Signal(
+            std::initializer_list<int> signals, bool block = true, EventQueue& eventQueue = EventQueue::coreInstance())
+            : Signal(createSigSet(signals), block, eventQueue) {}
+
+        void onSignal(std::function<void(signalfd_siginfo&&)> func) const;
+
+    private:
+        Fd m_fd;
+    };
+} // namespace suc::epl
+
+#endif // SUC_EPL_SIGNAL_HXX
---
+++ epl/src/common/Signal.cxx
@@ -0,0 +1,64 @@
+// Copyright [2026] [maggu2810]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+// Created by maggu2810 on 1/30/26.
+//
+
+#include "suc/epl/common/Signal.hxx"
+
+#include "suc/cmn/runtimeerror_errno.hxx"
+#include <csignal>
+
+namespace suc::epl {
+    [[nodiscard]] sigset_t Signal::createSigSet(std::initializer_list<int> signals) {
+        sigset_t sigset;
+        if (sigemptyset(&sigset) != 0) {
+            throw suc::cmn::runtimeerror_errno("sigemptyset failed");
+        }
+        for (const auto& sig : signals) {
+            if (sigaddset(&sigset, sig) != 0) {
+                throw suc::cmn::runtimeerror_errno("sigaddset failed");
+            }
+        }
+        return sigset;
+    }
+
+    void Signal::blockSignals(const sigset_t& sigset) {
+        if (sigprocmask(SIG_BLOCK, &sigset, nullptr) != 0) {
+            throw suc::cmn::runtimeerror_errno("sigprocmask failed");
+        }
+    }
+
+    Signal::Signal(const sigset_t& sigset, EventQueue& eventQueue)
+        : m_fd{suc::cmn::fd::make_or_rteeno(signalfd(-1, &sigset, SFD_NONBLOCK | SFD_CLOEXEC)), eventQueue} {}
+
+    void Signal::onSignal(std::function<void(signalfd_siginfo&&)> func) const {
+        m_fd.onInputAvailable([func = std::move(func), this]() {
+            signalfd_siginfo info[32];
+            const auto rv_read = read(m_fd, info, sizeof(info));
+            if (rv_read > 0) {
+                for (int i = 0; i < rv_read / sizeof(*info); ++i) {
+                    func(std::move(info[i]));
+                }
+            } else {
+                if (rv_read == 0) {
+                    throw suc::cmn::runtimeerror_errno("read of signalfd returns 0");
+                } else {
+                    throw suc::cmn::runtimeerror_errno("read of signalfd failed");
+                }
+            }
+        });
+    }
+} // namespace suc::epl
